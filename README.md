628857c1d2c58b864e44e571a4e6a120d4efefbd
Brandon Perard <brandon.perard@gmail>
2021-01-13 18:17:11 -0800

    update example.test.js with needed outcomes, add needed function outlines to README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # lab-03-rock-paper-scissors
+
+// Functions
+- getRandomThrow
+ > generate throw for computer opponent
+ > Math.floor(Math.random() * 3)
+  * paper = 0
+  * scissors = 1
+  * rock = 2
+
+- didUserWin
+ > compare user & computer throw
+ > if user === computer
+  * draw
+  * else user + computer = decision
+ > if decision = 3
+  * return (user === 2(rock)) ? 'win' : 'loss';
+ > if decision = 2
+  * return (user === 0(paper)) ? 'win' : 'loss';
+ > else (decision = 1)
+  * return (user === 1(scissors)) ? 'win' : 'loss';
+
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,66 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { didUserWin } from './utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+test('function takes in (2,1) & ', (expect) => {
+    const expected = 'win';
+    const actual = didUserWin(2, 1);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('function takes in (2,0) & ', (expect) => {
+    const expected = 'loss';
+    const actual = didUserWin(2, 0);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (2,2) & ', (expect) => {
+    const expected = 'draw';
+    const actual = didUserWin(2, 2);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (1,2) & ', (expect) => {
+    const expected = 'loss';
+    const actual = didUserWin(1, 2);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (1,1) & ', (expect) => {
+    const expected = 'draw';
+    const actual = didUserWin(1, 1);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (1,0) & ', (expect) => {
+    const expected = 'win';
+    const actual = didUserWin(1, 0);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (0,2) & ', (expect) => {
+    const expected = 'win';
+    const actual = didUserWin(0, 2);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (0,1) & ', (expect) => {
+    const expected = 'loss';
+    const actual = didUserWin(0, 1);
+
+    expect.equal(actual, expected);
+});
+
+test('function takes in (0,0) & ', (expect) => {
+    const expected = 'draw';
+    const actual = didUserWin(0, 0);
+
     expect.equal(actual, expected);
 });
