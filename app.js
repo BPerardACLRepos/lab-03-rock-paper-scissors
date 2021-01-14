d0907af1e6ac6b201b47f0ba0271f171b283f5f0
Brandon Perard <brandon.perard@gmail>
2021-01-13 20:50:51 -0800

    add images to assets folder and constants/states to app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-// import functions and grab DOM elements
+import { didUserWin } from './utils.js';
 
-// initialize state
+//buttons
+const throwButton = document.querySelector('#throw-button');
+const resetButton = document.querySelector('#reset-button');
+//sections
+const gameControlsSection = document.querySelector('#game-controls-section');
+const userNotificationSection = document.querySelector('#user-notification-section');
+//spans
+const winsSpan = document.querySelector('#wins-span');
+const lossesSpan = document.querySelector('#losses-span');
+const drawsSpan = document.querySelector('#draws-span');
+const totalGamesSpan = document.querySelector('#total-games-span');
+//img
+const playerThrowImg = document.querySelector('#player-throw-img');
+const computerThrowImg = document.querySelector('#computer-throw-img');
 
-// set event listeners to update state and DOM
+const endgameMessage = document.querySelector('#endgame-message');
+
+let totalWins = 0;
+let totalLosses = 0;
+let totalDraws = 0;
+
